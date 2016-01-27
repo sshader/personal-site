36eaa5512a3fafd74d5cc13d3137b94bfbf6abb3
Sarah <sshader@mit.edu>
2016-01-27 07:52:18 -0500

    Whoo

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { background: 'index', title: 'Sarah Shader' });
+});
+
+router.get('/experience', function(req, res, next) {
+	res.render('experience', { background: 'experience', 
+		title: 'Sarah Shader' });
+})
+
+module.exports = router;
